e339cc7a5695697d9e1a0588d50e593abb8bd363
Alissonoliveir <102183299+Alissonoliveir@users.noreply.github.com>
2022-04-19 21:26:15 -0300

    adiciona a atividade feita no encontro remoto

---
+++ encontroremotoSA3.js
@@ -0,0 +1,15 @@
+let alunos = 16;
+for(let i = 0; i <= alunos; i++){
+    if( i == 0){
+        console.log(`zero ${i}`);
+    }else if(i % 2 == 0){
+        console.log(`par: ${i}`);
+    }else{
+        console.log(`impar:${i}`);
+    }
+
+
+
+
+
+}
